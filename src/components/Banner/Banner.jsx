8058ca74b5eacf687acacadaaab72154ad2a6fe6
codeswithrohit <hrohit320@gmail.com>
2024-09-19 16:59:32 +0530

    submitted

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import sliderImg from "../../assets/images/slider/1.png";
+import sliderImg1 from "../../assets/images/slider/2.png";
+import "../Banner/banner.css"
+
+const Banner = () => {
+  return (
+    <>
+      <section className="slider">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <img src="https://as2.ftcdn.net/v2/jpg/04/70/68/73/1000_F_470687328_X5mqwze2IFjvzO62cTima3wdk9HjBzMC.jpg" className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  JOURNEY TO <span>EXPLORE MUSSORIE</span>
+                </h5>
+              
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src={sliderImg1} className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  BEAUTIFUL PLACE <span>TO VISIT</span>
+                </h5>
+                <p className="sub_text">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
